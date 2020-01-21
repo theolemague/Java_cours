659e17d9a1f963e2a44a1072d8a2965d5a377cb4
theolemague <60031565+theolemague@users.noreply.github.com>
2020-01-21 10:17:04 +0100

    Create GroupeEtudiant.java

---
+++ TD1 Conteneur/Ex1/GroupeEtudiant.java
@@ -0,0 +1,36 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+
+public class GroupeEtudiant{
+	
+	private ArrayList<Etudiant> listEtudiant = new ArrayList<Etudiant>();
+
+	/**
+     * Ajoute un etudiant dans la list du groupe
+     * @param e un etudiant 
+     * @return Une 'List<Etudiant>' contenant les etudiants ayant ce nom, vide si aucun ne l'a
+     */
+	public void ajout(Etudiant e){
+		listEtudiant.add(e);
+	}
+
+	/**
+     * Indique si le nom de l'etudiant est compris dans le groupe d'etudiant
+     * @param nom un nom donne
+     * @return Une 'List<Etudiant>' contenant les etudiants ayant ce nom, vide si aucun ne l'a
+     */
+	public ArrayList<Etudiant> recherche(String nom){
+		ArrayList<Etudiant> result = new ArrayList<Etudiant>();
+
+		for ( Etudiant e : listEtudiant ) {
+			if (e.aCeNom(nom)){
+				result.add(e);
+			} 
+		}
+		return result;
+	}
+
+}
